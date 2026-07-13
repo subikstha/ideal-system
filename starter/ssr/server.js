748aaf68492b8c07f4d582dbea567e193183b938
subikstha <subsatch@gmail.com>
2026-07-13 20:43:50 +0545

    feat: ssr from scratch

---
+++ starter/ssr/server.js
@@ -0,0 +1,34 @@
+import fastify from "fastify";
+import fastifyStatic from "@fastify/static";
+import { readFileSync } from "node:fs";
+import { fileURLToPath } from "node:url";
+import path, { dirname } from "node:path";
+import { renderToString } from "react-dom/server";
+import { createElement as h } from "react";
+import App from "./App.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// We grab the built version bcoz the built version will have the correct script tags
+const shell = readFileSync(path.join(__dirname, "dist", "index.html"), "utf-8");
+
+const app = fastify();
+
+app.register(fastifyStatic, {
+  root: path.join(__dirname, "dist"),
+  prefix: "/",
+});
+
+const parts = shell.split("<!--ROOT-->");
+app.get("/", (req, reply) => {
+  reply.raw.write(parts[0]);
+  const reactApp = renderToString(h(App));
+  reply.raw.write(reactApp);
+  reply.raw.write(parts[1]);
+  reply.raw.end();
+});
+
+app.listen({
+  port: 3000,
+});
