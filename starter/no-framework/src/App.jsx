a443e3f4e8ec83785c40cb83fb98fd68cf1f2504
subikstha <subsatch@gmail.com>
2026-07-14 11:53:07 +0545

    feat: RSC without framework setup

---
+++ starter/no-framework/src/App.jsx
@@ -0,0 +1,14 @@
+import { Suspense } from "react";
+import ServerComponent from "./ServerComponent";
+import ClientComponent from "./ClientComponent";
+
+export default function App() {
+  console.log("Rendering App server component");
+  return (
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <h1>Notes app</h1>
+      <ServerComponent />
+      <ClientComponent />
+    </Suspense>
+  );
+}
---
+++ starter/no-framework/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("node:path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ReactServerWebpackPlugin = require("react-server-dom-webpack/plugin");
+
+const mode = process.env.NODE_ENV || "development";
+const development = mode === "development";
+
+const config = {
+  mode,
+  entry: "./src/Client.jsx",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/, // This means for anything that ends with js or jsx run through babel
+        use: "babel-loader",
+        exclude: /node-modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      publicPath: "/assets/",
+      template: "./index.html",
+    }),
+    new ReactServerWebpackPlugin({
+      isServer: false,
+    }),
+  ],
+  output: {
+    chunkFilename: development
+      ? "[id].chunk.js"
+      : "[id].[contenthash].chunk.js",
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: "single",
+  },
+};
+
+module.exports = config;
