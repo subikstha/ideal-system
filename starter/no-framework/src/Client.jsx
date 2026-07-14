1e1fc030e8986bab0336ed14bed08a1dffb19fb3
subikstha <subsatch@gmail.com>
2026-07-14 16:00:52 +0545

    feat: created server and client components for RSC

---
+++ starter/no-framework/src/Client.jsx
@@ -0,0 +1,10 @@
+import { createRoot } from "react-dom/client";
+import { createFromFetch } from "react-server-dom-webpack/client";
+import "doodle.css/doodle.css";
+
+console.log("Fetching flight response");
+const fetchPromise = fetch("/react-flight");
+const root = createRoot(document.getElementById("root"));
+const p = createFromFetch(fetchPromise);
+console.log("rendering root", p);
+root.render(p);
---
+++ starter/no-framework/src/ClientComponent.jsx
@@ -0,0 +1,15 @@
+"use client";
+import { useState } from "react";
+
+export default function ClientComponent() {
+  console.log("Rendering client component");
+  const [counter, setCounter] = useState(0);
+
+  return (
+    <fieldset>
+      <legend>Client Component</legend>
+      <p>Counter {counter}</p>
+      <button onClick={setCounter(counter + 1)}>Increment</button>
+    </fieldset>
+  );
+}
